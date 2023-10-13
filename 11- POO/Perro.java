772df13993b6eb24e3308cc463ff608d9891b967
Felipe0521 <felipitocard0521@gmail.com>
2023-10-13 12:15:31 -0500

    clases perro

--- 11- POO/Perro.java
+++ 11- POO/Perro.java
@@ -6,24 +6,27 @@ public class Perro {
     private String raza;
     private double peso;
     private int energia;
+    private String estado;
 
-    public Perro( String nombre,String propietario, int edad,String raza,double peso,int energia){
+    public Perro( String nombre,String propietario, int edad,String raza,double peso,int energia, String estado){
       this.nombre = nombre;
       this.propietario = propietario;
        this.edad = edad;
        this.raza = raza;
        this.peso = peso;
        this.energia = energia;  
+       this.estado = estado;
     }
 
     public void imprimirDetalle(){
         System.out.println("+-----------------------------------+");
-         System.out.println("| nombre: "+nombre);
-         System.out.println("| propietario: "+propietario);
+         System.out.println("| Nombre: "+nombre);
+         System.out.println("| Propietario: "+propietario);
          System.out.println("| Edad: "+edad);
-         System.out.println("| raza: "+raza);
-         System.out.println("| peso: "+peso);
-         System.out.println("| energia: "+energia);
+         System.out.println("| Raza: "+raza);
+         System.out.println("| Peso: "+peso);
+         System.out.println("| Energia: "+energia);
+         System.out.println("| Estado: "+estado);
        System.out.println("+-----------------------------------+");
     }
 
@@ -49,6 +52,10 @@ public class Perro {
         return energia;
     }
 
+      public String getEstado(){
+        return estado;
+    }
+ // setters//
     public void setNombre(String dato){
         nombre= dato;
     }
@@ -69,10 +76,73 @@ public class Perro {
      public void setEnergia(int dato){
         energia= dato;
     }
+     public void setEstado(String dato){
+        estado = dato;
+    }
 
+    // Comer, caminar, ladrar, morder, dormir, hacerpopis, jugar //
+    
+    /*estado: el perro puede tener varios estados dependiendo del cambio de atributos o acciones
+    , sano, enfermo, cansado, hambiento, somnoliento, estresado  */
     public void comer(int gramos){
         peso = peso + ((double)gramos/1000);
 
         energia = energia + (gramos/100);
+
+        if (energia > 100) {
+            estado = "SOMNOLIENTO";
+        }if (energia > 50 && energia <100){
+            estado = "SANO";
+        }
+
+
+    }
+
+    public void caminar(int metros){
+        // peso 1 gramo por
+        
+        peso = peso -((double)metros/1000);
+
+        energia = energia -(metros/20);
+
+        if (energia < 50) {
+            estado = "CANSADO";
+        }if (energia < 20) {
+            estado = "CANSADO"+" HAMBRIENTO";
+        }
+    }
+
+    public void ladrar(int segundos ){
+        energia = energia-(segundos/50);
+        if (segundos > 30) {
+            estado = "Estresado"+ ", Furioso";
+            
+        }
+    }
+
+    public void morder(boolean morder, int segundos){
+        if (morder == true && segundos > 10) {
+            estado = "FURIOSO";
+            energia += 30;
+        }else{
+            estado = "Estresado";
+        }
+    }
+
+    public void dormir(double horas){
+        energia = energia + ((int)horas*10);
+        estado = "DURMIENDO";s
+    }
+
+    public void defecar(int gramos){
+        peso = peso - gramos;
+    }
+
+    public void jugar(int minutos){
+        energia = energia - (minutos /3);
+
+        if (energia < 50) {
+            estado = "CANSADO";
+        }
     }
 }
--- 11- POO/Principal.java
+++ 11- POO/Principal.java
@@ -17,18 +17,15 @@ public class Principal {
       perteneciante a la clase, Son funciones.
       */
 
-      Perro perro_01 = new Perro("Max", "valeria", 6, "Chihuaha", 30.5, 80);
+      Perro perro_01 = new Perro("Max", "valeria", 6, "Chihuaha", 30.5, 80,"SANO");
 
-       Perro perro_02 = new Perro("Mailo", "felipe", 18, "pug", 7, 20);
+       Perro perro_02 = new Perro("Mailo", "felipe", 18, "pug", 7, 20,"SANO");
 
       
        perro_01.comer(800);
       perro_01.imprimirDetalle();
         
 
-        //Podemos cogerle el bolso y hacerle la empanada//
-        //distraerlo y esconderle la botella //
-        // la tira del bolso amarrarla a la silla//
-        ///
+        
     }
 }
