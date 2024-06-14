687eccaab392fcafb29e0e2d1cea253a5992b072
FELIPE CARDONA <felipitocard0521@gmail.com>
2024-06-13 22:00:16 -0500

    Api musica

--- API-CANCIONES-PHP/Prueba-API-musica/API_musica/src/api/musica/APIMusica.java
+++ API-CANCIONES-PHP/Prueba-API-musica/API_musica/src/api/musica/APIMusica.java
@@ -17,7 +17,9 @@ public class APIMusica {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Menu menu = new Menu();
           
+        
       ConsumoAPI consumo = new ConsumoAPI();
       
        //endpoint obtener
@@ -26,7 +28,7 @@ public class APIMusica {
         
          String respuesta2 = consumo.consumoGET("http://localhost/02-APImusica/API/Obtener_categoria.php");
          System.out.println("Respuesta obtener:"+respuesta2);
-        
+        /*
         // Endpoint insertar cancion
         /*
         Map<String, String> datosInsertar = new HashMap<>();
@@ -38,7 +40,7 @@ public class APIMusica {
       
         String respuesta3 = consumo.consumoPOST("http://localhost/02-APImusica/API/Insertar_cancion.php",datosInsertar);
         System.out.println("Respuesta Insertar:"+respuesta3);
-        */
+        
         
          Map<String, String> datosInsertar = new HashMap<>();
         datosInsertar.put("codigo_genero", "04");
@@ -56,25 +58,25 @@ public class APIMusica {
         datosActualizar.put("genero", "02");
          String respuesta4 = consumo.consumoPOST("http://localhost/02-APImusica/API/Actualizar_cancion.php", datosActualizar);
          System.out.println("Respuesta Actualizar:"+respuesta4);
-    */
+    
           Map<String, String> datosActualizar = new HashMap<>();
         datosActualizar.put("codigo_genero", "03");
         datosActualizar.put("nombre_genero", "Punk");
        
          String respuesta4 = consumo.consumoPOST("http://localhost/02-APImusica/API/Actualizar_categoria.php", datosActualizar);
          System.out.println("Respuesta Actualizar:"+respuesta4);
-         /*
+         
          Map<String, String> datosEliminar = new HashMap<>();
          datosEliminar.put("codigo_cancion", "991212");
          String respuesta5 = consumo.consumoPOST("http://localhost/02-APImusica/API/Eliminar_cancion.php", datosEliminar);
          System.out.println("Respuesta Eliminar:"+respuesta5);
-       */
+       
           
          Map<String, String> datosEliminar = new HashMap<>();
          datosEliminar.put("codigo_genero", "04");
          String respuesta5 = consumo.consumoPOST("http://localhost/02-APImusica/API/Eliminar_categoria.php", datosEliminar);
          System.out.println("Respuesta Eliminar:"+respuesta5);
-       
+       */
     }
     
 }
